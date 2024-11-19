dbf58da7c5873a90a00e2686c7b8867f60cac409
SleekVortex <andretyklclash@gmail.com>
2024-11-19 10:51:32 +0700

    Добавлено усечение выбросов, генерация фичи для nlp модели, вынесены константы в отдельный файл

--- production/data_preparation.py
+++ production/data_preparation.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 from rapidfuzz import fuzz, utils
-from category_dicts import car_brand_to_country, subject_to_state_of_russia
+from constants import (
+    CAR_BRANDS_TO_COUNTRY, SUBJECTS_TO_STATE_OF_RUSSIA,
+    DUMMY_FEATURES, MODEL_FEATURES, PRICE_CLASSES_AMOUNT
+)
 
 from sklearn.model_selection import train_test_split
 
@@ -10,38 +13,38 @@ df_full = pd.read_csv('../data/cars_data_clear.csv')
 
 # Функция для извлечения федерального округа места продажи авто
 def get_region(adress):
-    for subregion in subject_to_state_of_russia.keys():
+    for subregion in SUBJECTS_TO_STATE_OF_RUSSIA.keys():
         if (
             subregion in adress
             or fuzz.WRatio(subregion, adress, processor=utils.default_process) >= 90
         ):
-            return subject_to_state_of_russia.get(subregion)
+            return SUBJECTS_TO_STATE_OF_RUSSIA.get(subregion)
     return 'Другое'
 
 df_full['region'] = df_full['city'].apply(get_region)
 
 # Функция для извлечения марки автомобиля
 def extract_brand(model):
-    for brand in car_brand_to_country.keys():
+    for brand in CAR_BRANDS_TO_COUNTRY.keys():
         if (
             brand in model
             or fuzz.token_set_ratio(brand, model, processor=utils.default_process) >= 95
         ):
-            return car_brand_to_country.get(brand)
+            return CAR_BRANDS_TO_COUNTRY.get(brand)
     return 'Другое'
 
 df_full['brand'] = df_full['model'].apply(extract_brand)
 df_full['is_dealer'] = df_full['is_dealer'].apply(lambda x: x=='TRUE')
 
-numeric_columns = [
+numeric_features = [
     'year', 'engine',
     'mileage', 'power_horse',
     'price'
 ]
 
-df_full[numeric_columns] = df_full[numeric_columns].apply(pd.to_numeric)
+df_full[numeric_features] = df_full[numeric_features].apply(pd.to_numeric)
 
-features_ml = [
+features_need = [
     'price', 'ad_description', 'is_dealer',
     'year', 'engine', 'transmission',
     'mileage', 'power_horse', 'car_body',
@@ -49,26 +52,28 @@ features_ml = [
     'region'
 ]
 
-df_full = df_full[features_ml]
+df_full = df_full[features_need]
 
-df_full.columns = [
-    'price', 'text', 'is_dealer',
-    'year', 'engine', 'transmission',
-    'mileage', 'power_horse', 'car_body',
-    'wheel_drive', 'fuel_type', 'brand',
-    'region'
-]
+df_full.columns = MODEL_FEATURES
 
-dummy_features = [
-    'transmission', 'car_body',
-    'wheel_drive','fuel_type', 'brand', 
-    'region'
-]
-
-df_full = pd.get_dummies(df_full, columns=dummy_features, drop_first=True)
+# КОдируем категориальные переменные
+df_full = pd.get_dummies(df_full, columns=DUMMY_FEATURES, drop_first=True)
 
 df_train, df_test = train_test_split(df_full, test_size=0.25, random_state=42)
 
+# Усекаем выбросы для трейна
+df_train = df_train[df_train.price <= df_train.price.quantile(0.999)]
+df_train = df_train[df_train.price >= df_train.price.quantile(0.001)]
+
+# Переводим непрерывную цену в категории цены, для nlp модели
+df_train['price_class'] = pd.qcut(
+    df_train['price'], q=PRICE_CLASSES_AMOUNT, labels=range(0, PRICE_CLASSES_AMOUNT)
+)
+df_test['price_class'] = pd.qcut(
+    df_test['price'], q=PRICE_CLASSES_AMOUNT, labels=range(0, PRICE_CLASSES_AMOUNT)
+)
+
+# Сохраняем все в папку data
 df_full.to_csv('../data/ml_data.csv', index=False)
 df_train.to_csv('../data/train_data.csv', index=False)
 df_test.to_csv('../data/test_data.csv', index=False)
